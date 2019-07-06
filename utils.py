7c532ff781e45c1c8aaccc43f5cfd51e1d3274a6
zhusleep <zhuflower@qq.com>
2019-07-06 21:07:52 +0800

    fix

--- utils.py
+++ utils.py
@@ -83,12 +83,15 @@ def calc_f1(pred,label,dev,ner_list):
 def split_list(item, n):
     new_item = []
     i = 0
+    item_tail = item[-2:]
+    item_head = item[0:-2]
     while True:
-        new_item.append(item[n*i:n*(i+1)])
-        if n*(i+1) >= len(item):
+        new_item.append(item_head[n*i:n*(i+1)])
+        if n*(i+1) >= len(item_head):
             break
         else:
             i += 1
+    new_item.append(item_tail)
     return new_item
 
 
